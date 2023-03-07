41df3275a64da74e00dee73634fb4c95e4306fdf
Maurin Schucan <maurin@schucan.ch>
2023-03-07 11:33:39 +0100

    add all methods to dataservice

--- youreLate/app/src/main/java/ch/zli/yourelate/DataService.java
+++ youreLate/app/src/main/java/ch/zli/yourelate/DataService.java
@@ -1,12 +1,29 @@
 package ch.zli.yourelate;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Binder;
+import android.os.Build;
 import android.os.IBinder;
+
+import androidx.core.app.NotificationCompat;
+
+import java.time.LocalTime;
 
 public class DataService extends Service {
     private final IBinder binder = new LocalBinder();
+
+    private static final String CHANNEL_ID = "defaultChannel";
+    private static final String CHANNEL_NAME = "Default Channel";
+
+    private NotificationManager notificationManager;
+
+    private double xcoordinate;
+    private double ycoordinate;
+    private LocalTime time;
 
     public class LocalBinder extends Binder {
         DataService getService() {
@@ -16,10 +33,48 @@ public class DataService extends Service {
     }
 
     public DataService() {
+        this.notificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 
     @Override
     public IBinder onBind(Intent intent) {
         return binder;
     }
+
+    public double getXcoordinate() {
+        return xcoordinate;
+    }
+    public void setXcoordinate(double xcoordinate) {
+        this.xcoordinate = xcoordinate;
+    }
+    public double getYcoordinate() {
+        return ycoordinate;
+    }
+    public void setYcoordinate(double ycoordinate) {
+        this.ycoordinate = ycoordinate;
+    }
+    public LocalTime getTime() {
+        return time;
+    }
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
+
+    private void checkTime(){
+        sendNotification();
+    }
+
+    private void sendNotification() {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(android.R.drawable.ic_media_play)
+                .setContentTitle("Late")
+                .setContentText("You are Late!!!")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        notificationManager.notify(0, builder.build());
+    }
+
 }
