e84fefd95101ec3bf4b72ceb52c1b1b4e2b56429
Maurin Schucan <maurin@schucan.ch>
2023-03-07 09:36:58 +0100

    create time view

---
+++ youreLate/app/src/main/java/ch/zli/yourelate/TimeFragment.java
@@ -0,0 +1,47 @@
+package ch.zli.yourelate;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ch.zli.yourelate.databinding.FragmentFirstBinding;
+import ch.zli.yourelate.databinding.FragmentTimeBinding;
+
+public class TimeFragment extends Fragment {
+
+    private FragmentTimeBinding binding;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentTimeBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        /*
+        binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(TimeFragment.this)
+                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+            }
+        });*/
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
